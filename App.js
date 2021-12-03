8da88d29a7226231029a5702ba0ee97c0320fe90
MUSAKTECH <92107770+MUSAKTECH@users.noreply.github.com>
2021-12-03 13:03:14 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,55 @@
+// import logo from './logo.svg'
+// import './App.css';
+// import React from 'react'
+// import { unstable_concurrentAct } from 'react-dom/test-utils';
+// import ReactDOM from 'react-dom'
+
+// ReactDOM.render(
+//   <ul>
+//   <li>1</li>
+// </ul>,document.getElementById("demo")
+// )
+// const Yes = <Car brand = "red"/>;
+// const Of = <Car new = "new" />;
+// function Car(props) {
+  // return(
+    // <h1>I am a {props.brand} Car</h1>
+  // );
+// }
+// ReactDOM.render(Yes, document.getElementById("demo"));
+
+// function App() {
+//   const bgcolor = {
+//     backgroundColor:"skyBlue",
+//   }
+//   return (
+//     <div className="App">
+//       <h1 style = {{backgroundColor:"Orange"}}>WELCOME TO FIRST TECHNICAL UNIVERSITY,IBADAN</h1>
+//       <form action="" className = "formleft">
+//                 <h4>FirstName</h4>
+//                      <input type="text" name="" id="" style = {bgcolor} autoFocus/>
+//                  <h4>Middle Names(Optional)</h4>
+//                      <input type="text" name="" id="" style = {bgcolor} autoFocus/>
+//                  <h4>Surname</h4>
+//                      <input type="text" name="" id="" style = {bgcolor} autoFocus/>
+//              </form>
+    
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
---
+++ index.js
@@ -0,0 +1,152 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
+
+// ReactDOM.render(
+  // const Yes = <Car brand = "red"/>;
+  // const Yeah = <Car new = "New" />
+  // const Car = (props) => {
+    // return(
+      // <h2>The Car is a {props.new} car</h2>
+    // );
+  // }
+  // function Car (){
+    // return(
+      // <>
+    {/* <Car new = "new" /> */}
+    // </>
+    // );
+  // }
+  // function Car(props) {
+  //   return (
+  //     <h2>I am a new {props.brand} Car</h2>
+  //   );
+  // }
+  
+  // const Hi = () => {
+  //   const hello = "Ford";
+  //   return(
+  //     <>
+  //     <Car brand = {hello}/>
+  //     </>
+  //   );
+  // }
+  // ReactDOM.render(<Hi/>, document.getElementById("root"));
+//   function Car(props) {
+//     return(
+//     <h1>I am a {props.brand} Car</h1>
+//   );
+// }
+// ReactDOM.render(Yes, document.getElementById("root"));
+
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>,
+  // document.getElementById('root')
+// );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
+
+//how to create Object and pass it to the Car Componenet
+// function Mr(props) {
+//   return (
+//     <h1>The Car is a {props.properties.Year} One</h1>
+//   );
+// }
+
+// function Mrs() {
+//   const child = {Color:"red", Year:2021};
+//   return(
+//     <>
+//     <h2>The nature of the Car are</h2>
+//     <Mr properties = {child} />
+//     </>
+//   );
+// }
+// ReactDOM.render(<Mrs/>, document.getElementById("root"));
+
+
+//React Event handler,This is done in the same way as that of HTML event handler.
+//creating a function for performing a task when a button is clicked!
+
+// function Football () {
+//     // <h1>On clicking the button,it shows what a score is; {props.alert}</h1>
+// const Scores = (a) => {
+//   alert(a);
+// }
+// return(
+//   <button onClick = {(a) => Scores("The Score is: 3-0")}>Check the Score by Clicking the button</button>
+// );
+// }
+// ReactDOM.render(<Football />, document.getElementsById("root"));
+
+// function Football1() {
+//   const Results = () => {
+//     alert("The score is unbeatable");
+//   }
+//   return(
+//     <button onClick = {Results}>Click to see the Scores</button>
+//   );
+// }
+
+
+
+// function is_a_goal(params) {
+  
+    
+    
+  // );
+// }
+
+//React testing for Authentications
+
+// function Authenticated() {
+// return <h1>You are In Successfully!</h1>
+// }
+// function DisAuthenticated() {
+//   return <h1>You details aew Incorrect!!</h1>
+// }
+
+//     let Authenticated = true;
+//       if (Authenticated){
+//         ReactDOM.render(<Authenticated/>, document.getElementsById("root"))
+//     }
+//       ReactDOM.render(<DisAuthenticated/>, document.getElementsById("root"))
+
+
+// function MissedGoal() {
+// 	return <h1>MISSED!</h1>;
+// }
+
+// function MadeGoal() {
+// 	return <h1>GOAL!</h1>;
+// }
+
+// function Goal(props) {
+//   const isGoal = props.isGoal;
+//   if (isGoal) {
+//     return <MadeGoal/>;
+//   }
+//   return <MissedGoal/>;
+// }
+
+// ReactDOM.render(
+//   <Goal isGoal={true} />,
+//   document.getElementById('root')
+// );
+
+function App() {
+  const x = 
+  return(
+    <>
+    <h1>Hello sir</h1>
+    <p>He is the one there nah</p>
+     </>
+  );
+}
+ReactDOM.render(<App/>, document.getElementById('root'))
